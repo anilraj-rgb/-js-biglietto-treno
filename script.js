8ff1df1629391dc7f662d59c6ccff28938cbd69e
anilraj-rgb <anil.raj9727@gmail.com>
2025-03-17 17:04:12 +0100

    [UPD] create the variables declaration

---
+++ script.js
@@ -0,0 +1,8 @@
+// Variables declaration
+let price;
+const pricePerKm = 0.21;
+const numberKm = Number(
+    prompt("inserisci il numero di km che vuoi percorrere")
+);
+console.log(numberKm,typeof age);
+
