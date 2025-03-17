edb385f72bc2425c6fcff89bf8fbcd6726858aa0
anilraj-rgb <anil.raj9727@gmail.com>
2025-03-17 17:04:12 +0100

    [UPD] finish the first part

---
+++ script.js
@@ -0,0 +1,11 @@
+// VARIABLE DECLARATION
+let price;
+const pricePerKm = 0.21;
+const numberKm = Number(
+    prompt("inserisci il numero di km che vuoi percorrere")
+);
+console.log(numberKm,typeof age);
+
+const age = Number(prompt("Inserisci la tua età"));
+console.log(age, typeof age);
+
